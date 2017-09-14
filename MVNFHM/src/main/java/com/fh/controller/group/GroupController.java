fff6bb433654adb73d3bc1d3dbca0fcb11980427
loucg <loucg@IBM344-PBW16AZ.cn.ibm.com>
2017-09-14 20:23:19 +0800

    增加删除分组功能

--- MVNFHM/src/main/java/com/fh/controller/group/GroupController.java
+++ MVNFHM/src/main/java/com/fh/controller/group/GroupController.java
@@ -1,11 +1,14 @@
 package com.fh.controller.group;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fh.controller.base.BaseController;
@@ -13,66 +16,79 @@ import com.fh.entity.Page;
 import com.fh.entity.system.User;
 import com.fh.service.fhoa.department.DepartmentManager;
 import com.fh.service.group.GroupService;
+import com.fh.util.AppUtil;
 import com.fh.util.Const;
 import com.fh.util.Jurisdiction;
 import com.fh.util.PageData;
 
 /**
  * 分组设置
- * @author xiaozhou
- * 创建时间：2017年1月17日
+ * 
+ * @author xiaozhou 创建时间：2017年1月17日 @001 删除分组页面 add 2017/09/13
  */
 @Controller
-@RequestMapping(value="/group")
-public class GroupController extends BaseController{
+@RequestMapping(value = "/group")
+public class GroupController extends BaseController {
 
-	String menuUrl = "group/listGroups.do";		//页面配置的菜单地址
-    @Resource(name="groupService")
-    private GroupService groupService;
-    @Resource(name="departmentService")
-    private DepartmentManager departmentService;
-	
+	String menuUrl = "group/listGroups.do"; // 页面配置的菜单地址
+	@Resource(name = "groupService")
+	private GroupService groupService;
+	@Resource(name = "departmentService")
+	private DepartmentManager departmentService;
+
 	/**
 	 * 显示分组列表
+	 * 
 	 * @param page
 	 * @return
 	 */
 	@RequestMapping("/listGroups")
-	public ModelAndView listGroup(Page page) throws Exception{
+	public ModelAndView listGroup(Page page) throws Exception {
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
-		String name = pd.getString("name");			//检索条件：名称
-		if(null !=name && !"".equals(name)){
+		String name = pd.getString("name"); // 检索条件：名称
+		if (null != name && !"".equals(name)) {
 			pd.put("name", name.trim());
 		}
-		String explain = pd.getString("explain");		//检索条件：说明项
-		if(null !=explain && !"".equals(explain)){
+		String explain = pd.getString("explain"); // 检索条件：说明项
+		if (null != explain && !"".equals(explain)) {
 			pd.put("explain", explain.trim());
 		}
-		//获得登录的用户id
-		User user = (User)Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
+		String status = pd.getString("status"); // 检索条件：说明项
+		if (status == null) {
+			pd.put("status", "('1')");
+			status = "1";
+		} else if ("3".equals(status) || "".equals(status)) {
+			pd.put("status", "('1','2')");
+		} else if (null != status && !"".equals(status)) {
+			pd.put("status", "('" + status.trim() + "')");
+		}
+		// 获得登录的用户id
+		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
 		String sys_user_id = user.getUSER_ID();
 		pd.put("sys_user_id", sys_user_id);
-		
+
 		String userids = departmentService.getUseridsInDepartment(pd);
 		pd.put("userids", userids);
-		
+
 		page.setPd(pd);
-		List<PageData> groupList = groupService.listGroup(page);	//获取列表
+
+		List<PageData> groupList = groupService.listGroup(page); // 获取列表
 		mv.setViewName("groupmanage/groupmanage_list");
 		mv.addObject("groupList", groupList);
+		pd.put("status", status);
 		mv.addObject("pd", pd);
 		mv.addObject("QX", Jurisdiction.getHC());
-		
+
 		return mv;
 	}
-	
+
 	/**
 	 * 去新增分组页面
 	 */
 	@RequestMapping("/goAdd")
-	public ModelAndView goAdd() throws Exception{
+	public ModelAndView goAdd() throws Exception {
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
@@ -81,36 +97,64 @@ public class GroupController extends BaseController{
 		mv.addObject("pd", pd);
 		return mv;
 	}
-	
-	/**新增分组页面的保存
-	 * @param
-	 * @throws Exception
+
+	// @001 start
+	/**
+	 * 删除分组页面
 	 */
-	@RequestMapping(value="/save")
-	public ModelAndView save() throws Exception{
-		logBefore(logger, Jurisdiction.getUsername()+"新增group");
-		
-		if(!Jurisdiction.buttonJurisdiction(menuUrl, "add")){return null;} //校验权限
+	@RequestMapping("/goDel")
+	@ResponseBody
+	public Object goDel(Page page) throws Exception {
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
-		//获得登录的用户id
-		User user = (User)Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
+		String items = pd.getString("group_ids");
+		String arrayitems[] = items.split(",");
+		for (int i = 0; i < arrayitems.length; i++) {
+			groupService.deleteTerms(arrayitems[i]);
+		}
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("result", "success");
+		return AppUtil.returnObject(new PageData(), map);
+	}
+	// @001 END
+
+	/**
+	 * 新增分组页面的保存
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/save")
+	public ModelAndView save() throws Exception {
+		logBefore(logger, Jurisdiction.getUsername() + "新增group");
+
+		if (!Jurisdiction.buttonJurisdiction(menuUrl, "add")) {
+			return null;
+		} // 校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		// 获得登录的用户id
+		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
 		String sys_user_id = user.getUSER_ID();
 		pd.put("sys_user_id", sys_user_id);
-		
+
 		String userids = departmentService.getUseridsInDepartment(pd);
 		pd.put("userids", userids);
-		
-		//logBefore(logger, pd.getString("name")+pd.getString("explain")+pd.getString("type"));
+
+		// logBefore(logger,
+		// pd.getString("name")+pd.getString("explain")+pd.getString("type"));
 		groupService.addGroup(pd);
-		
-		mv.addObject("msg","success");
+
+		mv.addObject("msg", "success");
 		mv.setViewName("save_result");
 		return mv;
 	}
-	
-	/**去编辑分组页面
+
+	/**
+	 * 去编辑分组页面
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/goUpdate")
@@ -119,39 +163,41 @@ public class GroupController extends BaseController{
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
-		
+
 		pd = groupService.findById(pd);
 		mv.setViewName("groupmanage/groupmanage_edit");
 		mv.addObject("msg", "update");
 		mv.addObject("pd", pd);
-		
+
 		return mv;
 	}
-	
-	/**编辑分组页面的保存
+
+	/**
+	 * 编辑分组页面的保存
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	@RequestMapping("/update")
-	public ModelAndView update() throws Exception{
+	public ModelAndView update() throws Exception {
 		logBefore(logger, "be sure edit group");
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
-		//获得登录的用户id
-		User user = (User)Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
+		// 获得登录的用户id
+		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USER);
 		String sys_user_id = user.getUSER_ID();
 		pd.put("sys_user_id", sys_user_id);
-		
+
 		String userids = departmentService.getUseridsInDepartment(pd);
 		pd.put("userids", userids);
-		
-		logBefore(logger, pd.getString("name")+pd.getString("explain")+pd.getString("status"));
+
+		logBefore(logger, pd.getString("name") + pd.getString("explain") + pd.getString("status"));
 		groupService.updateGroup(pd);
-		
-		mv.addObject("msg","success");
+
+		mv.addObject("msg", "success");
 		mv.setViewName("save_result");
 		return mv;
 	}
-	
+
 }
