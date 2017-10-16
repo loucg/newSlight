65a279c73566d1eae1c2c6e311ac1543d8b81780
suzsX1C <suzsX1C@9.234.88.23>
2017-10-16 16:04:55 +0800

    commit by lou

--- MVNFHM/src/main/java/com/fh/service/map/C_clientManager.java
+++ MVNFHM/src/main/java/com/fh/service/map/C_clientManager.java
@@ -1,44 +1,87 @@
 package com.fh.service.map;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import com.fh.entity.Page;
 import com.fh.entity.map.c_client;
+import com.fh.entity.map.c_partmap;
 import com.fh.entity.map.c_term;
 import com.fh.entity.map.draw_client;
 import com.fh.util.PageData;
 
-/**	地图控制类
- * @author zjc
- * 修改日期：2017.1.29
+/**
+ * 地图控制类
+ * 
+ * @author zjc 修改日期：2017.1.29
  */
 public interface C_clientManager {
 
-	 public List<c_term> queryAllterm() throws Exception ;
-	 public List<c_client> queryAllterm_client(c_client p) throws Exception ;
-	 public int queryCountterm_client(c_client p) throws Exception ;
-	 public List<c_client> addClientMaker(c_client cc) throws Exception ; 
-	 public List<c_client> getTypenameByGroup(int groupname) throws Exception ;
-	 public List<c_client> getAddressByType(c_client cc) throws Exception ;
-	 public List<c_client> getClientnameByaddress(c_client cc) throws Exception ;
-	 public List<c_client> getClientigByname(c_client cc) throws Exception ;
-	 public List<c_client> getSearchClient(c_client cc) throws Exception ;
-	 public void updateClientAttr_brightness(c_client cc) throws Exception ;
-	 public void updateClientAttr_status(c_client cc)throws Exception ;
-	public void updateClientAttr_statusON(c_client cc) throws Exception ;
-	public void updateClientAttr_statusOff(c_client cc) throws Exception ;
-	public List<c_client> queryAllterm_gateway(c_client p) throws Exception ;
-	public List<c_client> querGatewayPower(c_client p) throws Exception ;
-	public List<String> querGatewayClient(c_client p) throws Exception ;
-	public int queryCountgateway(c_client p) throws Exception ;
-	public List<c_client> getTypenameByGroupGateway(int groupnameid) throws Exception ;
-	public List<c_client> getAddressByTypeGataway(c_client cc) throws Exception ;
-	public List<c_client> getClientnameByaddressGateway(c_client cc) throws Exception ;
-	public List<c_client> getClientigBynameGateway(c_client cc) throws Exception ;
-	public List<c_client> getSearchGateway(c_client cc) throws Exception ;
-	public List<c_client> getClientByDraw(List  list) throws Exception;
-	public List<c_client> getGatewayByDraw(List  list) throws Exception;
-	public void updateClientDraw_status(draw_client dc)throws Exception;
-	
-		
+	public List<c_term> queryAllterm() throws Exception;
+
+	public List<c_client> queryAllterm_client(c_client p) throws Exception;
+
+	public int queryCountterm_client(c_client p) throws Exception;
+
+	public List<c_client> addClientMaker(c_client cc) throws Exception;
+
+	public List<c_client> getTypenameByGroup(int groupname) throws Exception;
+
+	public List<c_client> getAddressByType(c_client cc) throws Exception;
+
+	public List<c_client> getClientnameByaddress(c_client cc) throws Exception;
+
+	public List<c_client> getClientigByname(c_client cc) throws Exception;
+
+	public List<c_client> getSearchClient(c_client cc) throws Exception;
+
+	public void updateClientAttr_brightness(c_client cc) throws Exception;
+
+	public void updateClientAttr_status(c_client cc) throws Exception;
+
+	public void updateClientAttr_statusON(c_client cc) throws Exception;
+
+	public void updateClientAttr_statusOff(c_client cc) throws Exception;
+
+	public void updateClientAttr_Coordinate(c_client cc) throws Exception;
+
+	public List<c_client> queryAllterm_gateway(c_client p) throws Exception;
+
+	public List<c_client> querGatewayPower(c_client p) throws Exception;
+
+	public List<String> querGatewayClient(c_client p) throws Exception;
+
+	public int queryCountgateway(c_client p) throws Exception;
+
+	public List<c_client> getTypenameByGroupGateway(int groupnameid) throws Exception;
+
+	public List<c_client> getAddressByTypeGataway(c_client cc) throws Exception;
+
+	public List<c_client> getClientnameByaddressGateway(c_client cc) throws Exception;
+
+	public List<c_client> getClientigBynameGateway(c_client cc) throws Exception;
+
+	public List<c_client> getSearchGateway(c_client cc) throws Exception;
+
+	public List<c_client> getClientByDraw(List list) throws Exception;
+
+	public List<c_client> getGatewayByDraw(List list) throws Exception;
+
+	public void updateClientDraw_status(draw_client dc) throws Exception;
+
+	public List<c_client> getALLClient(c_client cc) throws Exception;
+
+	public List<PageData> getCountClientfromPartMap(Page page) throws Exception;
+
+	public List<c_partmap> getPartMapInfo(c_partmap cp) throws Exception;
+
+	public void insertPartMapHeader(c_partmap cp) throws Exception;
+
+	public void updatePartMapHeader(c_partmap cp) throws Exception;
+
+	public void insertPartMapDetail(c_partmap cp) throws Exception;
+
+	public void delPartMapDetail(c_partmap cp) throws Exception;
+
+	public void delPartMapHeader(c_partmap cp) throws Exception;
+
 }
